893180eb5316f2f6eefaf5a2c53268822f95bfea
anonymousecode <anonymousecode1@gmail.com>
2025-08-20 20:58:36 +0530

    integrated firebase auth, added login page, moved api keys to env

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAVwZk6BgeZJZ1SMDNtJKL7-BPgYd69R5E",
-  authDomain: "ping00.firebaseapp.com",
-  projectId: "ping00",
-  storageBucket: "ping00.firebasestorage.app",
-  messagingSenderId: "396510865733",
-  appId: "1:396510865733:web:a144d479819075725e362c"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
+  
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
